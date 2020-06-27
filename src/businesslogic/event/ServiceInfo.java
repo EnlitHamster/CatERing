c16c5c07bf382956244c8d21604ef7f60e194a42
EnlitOwl <sandro.massa@edu.unito.it>
2020-06-27 17:33:25 +0200

    Fix

--- src/businesslogic/event/ServiceInfo.java
+++ src/businesslogic/event/ServiceInfo.java
@@ -12,6 +12,7 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceInfo implements EventItemInfo {
